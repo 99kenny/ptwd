d153d455e22c7fbdc57a491f4a0f3b3968bc769e
99kenny <helloiamkenny@gmail.com>
2023-10-16 00:04:42 +0900

    fix preNorm

--- pre_norm.py
+++ pre_norm.py
@@ -11,9 +11,9 @@ class PreNorm(nn.Module):
     def forward(self, x, is_pre):
         if is_pre:
             # x = (batch, size, dim) 
-            x = x[:,self.patch_num+1:-1,:]
-            self.mean = torch.mean(x, dim=(0,2)) # (size,)
-            self.var = torch.var(x, dim=(0,2))   # (size,)
+            stats = x[:,self.patch_num+1:-1,:]
+            self.mean = torch.mean(stats, dim=(0,2)) # (size,)
+            self.var = torch.var(stats, dim=(0,2))   # (size,)
             
         return x
         
